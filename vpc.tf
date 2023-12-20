732203a7e36aee2d81bf0dd4675be600e48b63b8
Taiwo <Bellotaiwoa30@gmail.com>
2023-12-20 14:41:23 +0100

    vpc

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+resource "google_compute_network" "vpc" {
+  name                    = "spanner-with-network"
+  auto_create_subnetworks = false
+
+  depends_on = [google_project_service.service_api]
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-with-spanner"
+  ip_cidr_range = "10.1.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.vpc.id
+}
