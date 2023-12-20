7cac994d9ce280a82204af74a5f190a7c1287c5c
Taiwo <Bellotaiwoa30@gmail.com>
2023-12-20 15:03:03 +0100

    variable

--- variable.tf
+++ variable.tf
@@ -9,3 +9,28 @@ variable "region" {
 variable "zone" {
   default = "us-central1-b"
 }
+
+variable "spanner_config" {
+  type = object({
+    instance_name       = string
+    database_name       = string
+    configuration       = string
+    display_name        = string
+    processing_units    = number
+    deletion_protection = bool
+  })
+  default = {
+    instance_name = "value"
+  }
+  description = "The configuration specifications for the Spanner instance"
+
+  validation {
+    condition     = length(var.spanner_config.display_name) >= 4 && length(var.spanner_config.display_name) <= "30"
+    error_message = "Display name must be between 4-30 characters long."
+  }
+
+  validation {
+    condition     = (var.spanner_config.processing_units <= 1000) && (var.spanner_config.processing_units%100) == 0
+    error_message = "Processing units must be 1000 or less, and be a multiple of 100."
+  }
+}
