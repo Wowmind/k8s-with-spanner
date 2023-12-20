16cf7b578b9c9406b60cde4c7c9b7bcabb610aba
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-20 20:15:04 +0100

    main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "google_spanner_instance" "instance" {
+  name             = var.spanner_config.instance_name  # << be careful changing this in production
+  config           = var.spanner_config.configuration
+  display_name     = var.spanner_config.display_name
+  processing_units = var.spanner_config.processing_units
+  labels           = {
+    "env" = var.resource_env_label
+  }
+
+  depends_on = [google_project_service.service_api]
+}
+
+resource "google_spanner_database" "database" {
+  instance            = google_spanner_instance.instance.name
+  name                = var.spanner_config.database_name
+  deletion_protection = var.spanner_config.deletion_protection
+}
