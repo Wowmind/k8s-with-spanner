362ba488c246060cc57a66f2094d574035610a85
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-20 23:15:49 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+//create spanner instance 
+
 resource "google_spanner_instance" "instance" {
   name             = var.spanner_config.instance_name  # << be careful changing this in production
   config           = var.spanner_config.configuration
@@ -10,6 +12,8 @@ resource "google_spanner_instance" "instance" {
   depends_on = [google_project_service.service_api]
 }
 
+// create spanner database
+
 resource "google_spanner_database" "database" {
   instance            = google_spanner_instance.instance.name
   name                = var.spanner_config.database_name
@@ -20,4 +24,38 @@ resource "google_spanner_database" "database" {
   extra_statements = [
     "ALTER DATABASE hello-database SET OPTIONS (default_leader = 'us-central1-a')"
   ]
+}
+
+// create gke autopilot
+
+resource "google_container_cluster" "sample-game-gke" {
+  name              = var.gke_config.cluster_name
+  location          = var.gke_config.location
+  network           = google_compute_network.vpc.name
+  subnetwork        = google_compute_subnetwork.subnet.name
+
+  # Use locked down service account
+  cluster_autoscaling {
+    auto_provisioning_defaults {
+      service_account = google_service_account.gke-sa.email
+    }
+  }
+
+  # Enabling Autopilot for this cluster
+  enable_autopilot  = true
+
+  resource_labels = {
+    "environment" = var.resource_env_label
+  }
+
+  depends_on = [google_service_account.gke-sa]
+}
+
+
+
+data "google_container_cluster" "gke-provider" {
+  name        = var.gke_config.cluster_name
+  location    = var.gke_config.location
+
+  depends_on  = [ google_container_cluster.sample-game-gke ]
 }
