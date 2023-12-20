8a88c2231812d1d0c50f84b5ab2a24667931cb7d
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-20 23:02:50 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -14,4 +14,10 @@ resource "google_spanner_database" "database" {
   instance            = google_spanner_instance.instance.name
   name                = var.spanner_config.database_name
   deletion_protection = var.spanner_config.deletion_protection
+  ddl_statements = [
+    "CREATE TABLE Players (PlayerUuid STRING(36) NOT NULL, FirstName STRING(1024), LastName STRING(1024), BirthDate DATE) PRIMARY KEY(PlayerUuid)"
+  ]
+  extra_statements = [
+    "ALTER DATABASE hello-database SET OPTIONS (default_leader = 'us-central1-a')"
+  ]
 }
