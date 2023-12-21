f9edcc62ffd9a0c3ba38e3a8d2b14dcc19f5e966
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-21 22:11:11 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -49,11 +49,16 @@ resource "google_container_cluster" "sample-game-gke" {
   depends_on = [google_service_account.gke-sa]
 }
 
-
-
 data "google_container_cluster" "gke-provider" {
   name        = var.gke_config.cluster_name
   location    = var.gke_config.location
 
   depends_on  = [ google_container_cluster.sample-game-gke ]
 }
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token                  = "${data.google_client_config.current.access_token}"
+  cluster_ca_certificate = "${base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)}"
+}
+
