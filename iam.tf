45691d463f9974a16d25434cf1de68c611ba10c1
Taiwo Bello <61393182+Wowmind@users.noreply.github.com>
2023-12-21 10:03:46 +0100

    iam.tf

---
+++ iam.tf
@@ -0,0 +1,16 @@
+resource "google_service_account" "gke-sa" {
+account_id    = "gke-cluster-account"
+  display_name  = "Service account to manage GKE Autopilot cluster"
+  project       = var.gcp_project
+}
+
+resource "google_project_iam_binding" "gke-identity-binding" {
+  project       = var.gcp_project
+  role                = "roles/container.nodeServiceAccount"
+
+  members = [
+     "serviceAccount:${google_service_account.gke-sa.email}",
+  ]
+
+  depends_on = [google_project_service.service_api, google_service_account.gke-sa]
+}
