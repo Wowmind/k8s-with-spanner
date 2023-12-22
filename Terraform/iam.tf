fcb8855a0275b65aba8072bfa1c6a717fddfc5e9
Taiwo <Bellotaiwoa30@gmail.com>
2023-12-22 13:10:52 +0100

    iam

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,6 +1,6 @@
-resource "google_service_account" "gke-sa" {
+resource "google_service_account" "gke-helloapp" {
 account_id    = "gke-cluster-account"
-display_name  = "Service account to manage GKE Autopilot cluster"
+display_name  = "gke-helloapp"
 project       = var.project_id
 }
 
@@ -13,4 +13,18 @@ resource "google_project_iam_binding" "gke-identity-binding" {
   ]
 
   depends_on = [google_project_service.service_api, google_service_account.gke-sa]
+}
+
+
+resource "google_service_account" "my_service_account" {
+  account_id   = "gsa-helloapp"
+  display_name = "My Service Account"
+}
+
+resource "google_project_iam_binding" "my_iam_binding" {
+  project = "var.project_id"
+  role    = "roles/spanner.admin"
+  members = [
+    "serviceAccount:${google_service_account.my_service_account.email}"
+  ]
 }
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -61,16 +61,3 @@ provider "kubernetes" {
   token                  = "${data.google_client_config.current.access_token}"
   cluster_ca_certificate = "${base64decode(data.google_container_cluster.my_cluster.master_auth.0.cluster_ca_certificate)}"
 }
-
-resource "google_service_account" "my_service_account" {
-  account_id   = "gsa-helloapp"
-  display_name = "My Service Account"
-}
-
-resource "google_project_iam_binding" "my_iam_binding" {
-  project = "var.project_id"
-  role    = "roles/spanner.admin"
-  members = [
-    "serviceAccount:${google_service_account.my_service_account.email}"
-  ]
-}
